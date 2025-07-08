8d38a72fd3b36f32171eeff80f65718de5af6917
David Sprankle <dsprankle@gmail.com>
2025-07-08 11:28:59 -0600

    bump

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Gridmoire</h1>
-        <p>A drag-and-drop character sheet creator for tabletop RPGs.</p>
+        <p>A drag-and-drop character sheet creator for tabletop RPGs...</p>
 
 
         <a
