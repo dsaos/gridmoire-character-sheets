67de990d62db3a913e1c16fd510d511c19da1706
David Sprankle <dsprankle@gmail.com>
2025-07-08 17:47:52 -0600

    styling landing page, meta

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,8 +1,15 @@
 import Header from "@/components/builder-ui/Header";
 import Canvas from "@/components/builder-ui/Canvas";
+import { Metadata } from "next";
 import { SheetConfig } from "@/types/sheet";
 
 import styles from './page.module.scss';
+
+
+export const metadata: Metadata = {
+  title: "Gridmoire Builder",
+  description: "Make your own intuitive and beautiful character sheets for tabletop RPGs like Dungeons & Dragons.",
+};
 
 export default function Builder() {
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
       <article>
-        <h1>Gridmoire</h1>
+        <h1>Gri<span style={{ color: '#a21313' }}>d</span>moire</h1>
         <h2>Custom Character Sheets</h2>
+        <hr />
         <p>A drag-and-drop interface for creating TTRPG character sheets to your own styling and specifications!</p>
         <p>Coming soon&mdash;support development by <a target="_blank" href="https://www.ko-fi.com/sprankledesign">donating to me on Ko-fi.</a></p>
         <p><small>&copy; 2025 Sprankle Design, LLC, all rights reserved.</small></p>
